2f64f6cff10b7259d3e35384a947616cc26e303c
Rafaela Mesquita <rafaela.mesquita@escola.pr.gov.br>
2025-05-16 22:31:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 function adicionarTarefa(){
     const input = document.getElementById('nova-tarefa');
     const textoTarefa = input.value.trim();
+
+    if (textoTarefa === ' ') return;
+
+    const li = document.createElement('li');
+    li.textContent = textoTarefa;
+
+    const botaoRemover = document.createElement('button');
+    botaoRemover.textContent = 'Remover';
+    botaoRemover.onclick = () => li.remove();
+
+    li.appendChild(botaoRemover);
+    document.getElementById('lista-tarefas').appendChild(li);
+
+    input.value = '';
 }
 
-if (textoTarefa === "") return;
-
--- paramos na linha 71 ----
